43dfaecdc8bae534dcf49da2a5c43540dfef56dc
khidr <akhidr@bitbucket.org>
2019-08-23 07:05:35 +0200

    script runs on startup

--- dbram.sh
+++ dbram.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-var1=~/runDBDocker.sh
-echo -e "#!/bin/bash \nsudo fuser -k 5432/tcp \nsudo docker start postgres96\n" > $var1
+var1=runDBDocker.sh
 sudo chmod 777 $var1
+sudo cp $var1 /etc/init.d/
+sudo update-rc.d $var1 defaults
 
