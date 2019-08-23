39368c94a273bc7c8e50236d08f2321241299e20
khidr <akhidr@bitbucket.org>
2019-08-23 06:18:09 +0200

    second commit

--- runDBDocker.sh
+++ runDBDocker.sh
@@ -6,12 +6,17 @@ if [[ $(docker images -q postgres:9.6) ]]; then
 	echo "postgres:9.6 image exists"
 else
 	echo "postgres:9.6 image does not exist"
-#	sudo docker pull postgres:9.6	
+	sudo docker pull postgres:9.6	
 fi
 
 if [ ! "$(docker ps -a | grep 'postgres96')" ]; then 
 	echo "container doesn't exist"
-	#sudo docker run --name postgres96 -p 5432:5432 --tmpfs /var/lib/postgresql/data:rw -e POSTGRES_PASSWORD=postgres -d postgres:9.6
+	if [ "$(sudo fuser 5432/tcp)" ]; then
+                echo "other services are running on port"
+                sudo fuser -k 5432/tcp && sudo docker run --name postgres96 -p 5432:5432 --tmpfs /var/lib/postgresql/data:rw -e POSTGRES_PASSWORD=postgres -d postgres:9.6
+        else
+                sudo docker run --name postgres96 -p 5432:5432 --tmpfs /var/lib/postgresql/data:rw -e POSTGRES_PASSWORD=postgres -d postgres:9.6
+        fi
 else
 	echo "container exists"
 fi
